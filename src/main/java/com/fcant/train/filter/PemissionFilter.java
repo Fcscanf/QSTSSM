498ee4a3745ac0db0ec32fe449504004f35ce0ef
Fcscanf <fcscanf@gmail.com>
2019-07-24 01:05:53 +0800

    [Codes-Fcant-Updates]:Updates:实现Filter过滤器对部分操作进行拦截过滤处理

---
+++ src/main/java/com/fcant/train/filter/PemissionFilter.java
@@ -0,0 +1,58 @@
+package com.fcant.train.filter;
+
+import com.fcant.train.bean.User;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * PemissionFilter
+ * <p>
+ * encoding:UTF-8
+ *
+ * @author Fcscanf
+ * @description
+ * @date 上午 0:51 2019-07-24/0024
+ */
+public class PemissionFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+// 将请求与响应向下转换
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
+        // 获得访问界面的url文件地址
+        String servletPath = req.getServletPath();
+        HttpSession session = req.getSession();
+        // 获取登录状态
+       User user = (User) session.getAttribute("user");
+        /* 判断是否是登录页、首页、登录servlet */
+        if (servletPath != null && (servletPath.equals("/jsp/userlogin.jsp") || servletPath.equals("/jsp/userreg.jsp"))) {
+            // 是则直接转发到下一组件
+            chain.doFilter(request, response);
+        } else {
+            // 否，则验证登录状态
+            if (user != null) {
+                chain.doFilter(request, response);
+            } else {
+                // 未登录，跳转到登录页，并保证当前网页的url文件路径
+                req.setAttribute("msg", "您尚未登录，请登录");
+                req.setAttribute("return_uri", servletPath);
+                RequestDispatcher rd = req.getRequestDispatcher("/jsp/userlogin.jsp");
+                rd.forward(req, res);
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
