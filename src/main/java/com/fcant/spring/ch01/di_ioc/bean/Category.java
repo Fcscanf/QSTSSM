e5c7fcbc536802d03fcc85f0d56af572134b033b
Fcscanf <fcscanf@gmail.com>
2019-07-30 08:58:34 +0800

    [Codes-Fcant-Updates-Spring]:Updates-Spring:Spring的注解方式-使用@Component注解注入JavaBean;依赖注入DI/IOC-JavaBean的注入

--- src/main/java/com/fcant/spring/ch01/di_ioc/bean/Category.java
+++ src/main/java/com/fcant/spring/ch01/di_ioc/bean/Category.java
@@ -1,6 +1,7 @@
 package com.fcant.spring.ch01.di_ioc.bean;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 /**
  * Category
@@ -12,7 +13,8 @@ import lombok.Data;
  * @date 下午 20:51 2019-07-29/0029
  */
 @Data
+@Component("c")
 public class Category {
     private int id;
-    private String name;
+    private String name = "category 1";
 }
--- src/main/java/com/fcant/spring/ch01/di_ioc/bean/Product.java
+++ src/main/java/com/fcant/spring/ch01/di_ioc/bean/Product.java
@@ -1,6 +1,7 @@
 package com.fcant.spring.ch01.di_ioc.bean;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
@@ -14,11 +15,12 @@ import javax.annotation.Resource;
  * @date 下午 20:56 2019-07-29/0029
  */
 @Data
+@Component("product")
 public class Product {
     private int id;
-    private String name;
-    private int number;
-    private double price;
-    @Resource(name="c")
+    private String name = "Apple Watch";
+    private int number = 2;
+    private double price = 8800;
+    @Resource(name = "c")
     private Category category;
 }
